d42138b68a8521ce35b634a916c4f690d6def3db
akashchauhan19 <chauhanakash19@gmail.com>
2022-08-17 21:49:28 +0530

    Load two CMS blocks on the checkout page

---
+++ app/code/Seepossible/Checkout/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Seepossible_Checkout
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Seepossible_Checkout',
+    __DIR__
+);
